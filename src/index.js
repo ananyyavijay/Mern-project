1dca4fa5a14dd1f22d351ed977b64d9d035c7330
ananyyavijay <ananyavijay05@gmail.com>
2025-10-04 01:11:28 +0530

    added util files

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ dotenv.config({
     path: './env'
 });
 
-connectDB();
-
-
-
-
-
-
-
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT | 8000, () => {
+        console.log(`Server is running on PORT: ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDb connection err: ", err);
+})
 
 
 
